9d9ae59d05da2fe56759de6ceedfee0c08ce8306
mono-nobe <ryoya.goemono@gmail.com>
2023-03-25 23:19:43 +0900

    不要な条件式を削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -52,7 +52,7 @@ class ReportsController < ApplicationController
     canonicalize_learning_times(@report)
     if @report.save
       Newspaper.publish(:report_create, @report.user)
-      Newspaper.publish(:report_save, @report) if report.saved_changes?
+      Newspaper.publish(:report_save, @report)
       redirect_to redirect_url(@report), notice: notice_message(@report), flash: flash_contents(@report)
     else
       render :new
@@ -66,7 +66,7 @@ class ReportsController < ApplicationController
     canonicalize_learning_times(@report)
     if @report.save
       Newspaper.publish(:report_update, @report.user)
-      Newspaper.publish(:report_save, @report) if report.saved_changes?
+      Newspaper.publish(:report_save, @report)
       redirect_to redirect_url(@report), notice: notice_message(@report), flash: flash_contents(@report)
     else
       render :edit
